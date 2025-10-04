fff83b22aabe331d9f319839c495d93be12b1a19
sajwansarthak <sarthaksajwan25@gmail.com>
2025-10-05 05:06:01 +0530

    Adding week-1 files

---
+++ week-4.1/index1.js
@@ -0,0 +1,13 @@
+function sum (a,b){
+    return a + b 
+}
+
+console.log(sum(2,3))
+
+//adding external package chalk
+
+
+// it will tell us the current directory 
+console.log(__dirname);
+
+// using path.join() we can join multiple paths together 
