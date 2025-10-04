fe53d07113cbab2966bec57ca67fa9e99ccabe68
sajwansarthak <sarthaksajwan25@gmail.com>
2025-10-04 07:38:35 +0530

    Adding DOM operations files

---
+++ week-3-dom/index.js
@@ -0,0 +1,9 @@
+/// updating element 
+
+// let ctr = 0;
+// function callback(){
+//     document.querySelectorAll("h4")[1].innerHTML=ctr
+//     ctr = ctr +1
+// }
+
+// setInterval(callback,1000);
