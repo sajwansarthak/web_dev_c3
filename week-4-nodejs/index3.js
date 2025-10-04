ae48e52072a0b6dfdd2c927f09c1c7c2a4d0aad0
sajwansarthak <sarthaksajwan25@gmail.com>
2025-10-05 05:07:50 +0530

    Adding files for nodejs

---
+++ week-4-nodejs/index3.js
@@ -0,0 +1,35 @@
+// this is how to create cli command 
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
+
+program.command('count')
+  .description('Count the number of lines in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }else{
+        let words = 0 ;
+        for(let i = 0 ; i < data.length;i++){
+
+            if(data[i] === " "){
+                words ++
+            }
+        }
+        console.log(words+1)
+      }
+    //   } else {
+    //     const lines = data.split().length;
+    //     console.log(`There are ${lines} lines in ${file}`);
+    //   }
+    });
+  });
+
+program.parse();
